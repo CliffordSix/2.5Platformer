4084e7b44d63ba1af7a4a1c557da5c3b6f8eb752
Ben Sandwick <ben657s@gmail.com>
2016-07-22 19:00:28 +0100

    Started adding behaviours

---
+++ Assets/Behaviours/MovementBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementBehaviour : MonoBehaviour {
+
+    protected Rigidbody2D body;
+
+    public virtual void Start() {
+        body = transform.GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        
+    }
+}
---
+++ Assets/Monsters/Monster.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monster : MonoBehaviour {
+
+    Transform model;
+
+    public int health;
+    public int speed;
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
