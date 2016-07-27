51d937b05401159731dbf5544f0e86f4976664dd
Ben Sandwick <ben657s@gmail.com>
2016-07-27 16:43:40 +0100

    Start state manager

---
+++ Assets/Monsters/Behaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Behaviour : MonoBehaviour {
+
+	void Start()
+    {
+
+    }
+
+    void OnEnabled()
+    {
+
+    }
+
+    void OnDisabled()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Monsters/StateManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum State
+{
+    Idle,
+    Chasing,
+    Searching,
+    Fighting
+}
+
+public class StateManager : MonoBehaviour {
+
+    public Transform target;
+    public State initialState;
+    public List<Behaviour> idleBehaviours = new List<Behaviour>();
+    public List<Behaviour> chasingBehaviours = new List<Behaviour>();
+    public List<Behaviour> searchingBehaviours = new List<Behaviour>();
+    public List<Behaviour> fightingBehaviours = new List<Behaviour>();
+
+    protected List<Behaviour> currentBehaviours;
+    protected State currentState;
+
+    void Start()
+    {
+        SetState(initialState);
+    }
+
+    protected void SetState(State newState)
+    {
+        foreach (Behaviour behaviour in currentBehaviours)
+        {
+            behaviour.gameObject.SetActive(false);
+        }
+
+        switch (newState)
+        {
+            case State.Idle:
+                currentBehaviours = idleBehaviours;
+                break;
+
+            case State.Chasing:
+                currentBehaviours = chasingBehaviours;
+                break;
+
+            case State.Searching:
+                currentBehaviours = searchingBehaviours;
+                break;
+
+            case State.Fighting:
+                currentBehaviours = fightingBehaviours;
+                break;
+
+            default:
+                currentBehaviours = new List<Behaviour>();
+                break;
+        }
+
+        currentState = newState;
+        foreach (Behaviour behaviour in currentBehaviours)
+        {
+            behaviour.gameObject.SetActive(true);
+        }
+    }
+}
