9e2023bc699859d60a27bc53d3850ce64f3d64b1
Ben Sandwick <ben657s@gmail.com>
2016-08-26 12:49:54 +0100

    Change deck list layout direction, add placeholder buttons

--- Assets/UI/DeckList.cs
+++ Assets/UI/DeckList.cs
@@ -5,11 +5,10 @@ using System.Collections.Generic;
 public class DeckList : MonoBehaviour {
 
     public CollectionManager collection;
-    public Transform content_container;
+    public Transform[] rows = new Transform[2];
     public Transform preview_content_container;
 
     public GameObject deck_button_prefab;
-    public float deck_button_offset = 0.15f;
 
     void Start()
     {
@@ -21,8 +20,19 @@ public class DeckList : MonoBehaviour {
         LoadDecks();
     }
 
+    void ClearRow(int index)
+    {
+        List<GameObject> children = new List<GameObject>();
+        foreach (Transform child in rows[index]) children.Add(child.gameObject);
+        children.ForEach(child => Destroy(child));
+    }
+
     void LoadDecks()
     {
+        for(int i = 0; i < 2; i++)
+        {
+            ClearRow(i);
+        }
         for (int i = collection.deck_collection.Count - 1; i >= 0; i--)
         {
             GameObject deck = collection.deck_collection[i];
@@ -32,7 +42,9 @@ public class DeckList : MonoBehaviour {
             DeckButton script = button.GetComponent<DeckButton>();
             script.deck = deck.GetComponent<Deck>();
             script.card_preview_container = preview_content_container;
-            button.transform.SetParent(content_container, false);
+            int row = i % 2 == 0 ? 0 : 1;
+            Debug.Log(row);
+            button.transform.SetParent(rows[row], false);
         }
     }
 }
