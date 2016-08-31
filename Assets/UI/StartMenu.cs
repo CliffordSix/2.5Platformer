a198ace006b541626fdd36de210d2cebef136be5
Ben Sandwick <ben657s@gmail.com>
2016-08-31 15:30:12 +0100

    Move managers to 0,0,0

--- Assets/UI/StartMenu.cs
+++ Assets/UI/StartMenu.cs
@@ -4,16 +4,6 @@ using System.Collections;
 
 public class StartMenu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
     public void Play()
     {
         SceneManager.LoadScene("Dungeon");
