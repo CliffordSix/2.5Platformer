53e426229e99519b284088199b2624b9bd4f9375
ben sandwick <ben657s@gmail.com>
2016-09-07 16:19:51 +0100

    Enemy health on the canvas

--- Assets/Scripts/Damageable.cs
+++ Assets/Scripts/Damageable.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using UnityEditor;
-using System.Collections;
+using UnityEngine.UI;
 
 public class Damageable : MonoBehaviour {
 
+    public GameObject Healthbar;
     public float maxHealth = 0.0f;
     public int maxArmour = 0;
     public float invincibilityPeriod = 0.0f;
+
+    public float healthbarOffset = 3.0f;
 
     float health = 0;
     int armour = 0;
@@ -25,6 +28,27 @@ public class Damageable : MonoBehaviour {
         health = maxHealth;
         armour = maxArmour;
         rigidbody = GetComponent<Rigidbody2D>();
+        if (gameObject.layer == 14)
+        {
+            Healthbar = Instantiate(Healthbar);
+            Healthbar.transform.SetParent(GameObject.Find("Canvas").GetComponent<Transform>());
+        }
+        else
+            Healthbar = null;
+    }
+
+    public Vector2 GetScreenPosition(Transform transform, Canvas canvas)
+    {
+        RectTransform canvasRect = canvas.GetComponent<RectTransform>();
+
+        Vector2 viewportPosition = Camera.main.WorldToViewportPoint(transform.position);
+       
+        Vector2 world_screenPosition = new Vector2(
+        ((viewportPosition.x * canvasRect.sizeDelta.x) - (canvasRect.sizeDelta.x * 0.5f)),
+        ((viewportPosition.y * canvasRect.sizeDelta.y) - (canvasRect.sizeDelta.y * 0.5f)));
+        world_screenPosition.y += healthbarOffset;
+        Debug.Log(world_screenPosition);
+        return world_screenPosition;
     }
 
     public float GetHealth()
@@ -64,6 +88,23 @@ public class Damageable : MonoBehaviour {
     {
         if(untilVulnerable > 0)
             untilVulnerable -= Time.deltaTime;
+       
+    }
+
+    void LateUpdate()
+    {
+        if (Healthbar != null)
+        {
+            Healthbar.GetComponent<RectTransform>().anchoredPosition = GetScreenPosition(transform, (GameObject.Find("Canvas").GetComponent<Canvas>()));
+            Image[] bars = Healthbar.GetComponentsInChildren<Image>();
+            foreach(Image i in bars)
+            {
+                if(i.gameObject.name == "HealthFull")
+                {
+                    i.fillAmount = health / maxHealth;
+                }
+            }
+        }
     }
 
     [CustomEditor(typeof(Damageable))]
@@ -74,7 +115,9 @@ public class Damageable : MonoBehaviour {
             Damageable script = (Damageable)target;
 
             EditorGUILayout.LabelField("Health", script.health.ToString());
+            script.Healthbar = EditorGUILayout.ObjectField("Health Bar", script.Healthbar, typeof(Object), true) as GameObject;
             script.maxHealth = EditorGUILayout.FloatField("Max Health", script.maxHealth);
+            script.healthbarOffset = EditorGUILayout.FloatField("Health Bar Offset", script.healthbarOffset);
             script.invincibilityPeriod = EditorGUILayout.FloatField("Invincivility Period", script.invincibilityPeriod);
 
             script.takesKnockback = EditorGUILayout.Toggle("Take Knockback", script.takesKnockback);
