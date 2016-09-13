8b6dc299343d8cbba46d171e278d4fd7f3156541
Ben Sandwick <ben657s@gmail.com>
2016-09-13 18:52:10 +0100

    Add teleport script

---
+++ Assets/Behaviours/Teleport.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Behaviours
+{
+    [RequireComponent(typeof(Triggers.Collider))]
+    public class Teleport : MonoBehaviour
+    {
+        public Triggers.Collider trigger { get; set; }
+        public Transform endPoint;
+
+        bool lastActive = false;
+
+        void Start()
+        {
+            trigger = GetComponent<Triggers.Collider>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (trigger.IsActive() && !lastActive)
+                trigger.other.transform.position = endPoint.position;
+
+            lastActive = trigger.IsActive();
+        }
+    }
+}
