bbdc66d835bf564e935c7dff0fd613f104191eb8
Ben Sandwick <ben657s@gmail.com>
2016-09-14 17:03:40 +0100

    Add breakable vase

---
+++ Assets/Scripts/Breakable.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Breakable : MonoBehaviour {
+
+    public Trigger trigger;
+    public Animator animator;
+    public float animationOffset = 0.0f;
+
+    bool lastTrigger = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(trigger.IsActive() && !lastTrigger)
+        {
+            GetComponent<Rigidbody2D>().isKinematic = true;
+            Vector3 pos = transform.position;
+            pos.y += animationOffset;
+            transform.position = pos;
+            animator.Play("Broken");
+        }
+
+        lastTrigger = trigger.IsActive();
+	}
+}
