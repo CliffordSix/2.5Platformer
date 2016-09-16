1efc06c3b071038ebb9393df5701239fc54a688c
ben sandwick <ben657s@gmail.com>
2016-09-16 14:05:04 +0100

    Set up portcullis + switches

---
+++ Assets/Behaviours/LerpToTarget.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LerpToTarget : MonoBehaviour {
+
+    public Transform target;
+    public float speed;
+	
+    public void SetTarget(Transform target)
+    {
+        this.target = target;
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 direction = (target.position - transform.position).normalized;
+        transform.position += direction * speed;
+	}
+}
